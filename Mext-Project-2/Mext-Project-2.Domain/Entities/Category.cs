3eea9f893e99173a0dcfee2166330d16f46a742b
Aysegul <karadan.ay7@gmail.com>
2024-05-27 15:25:00 +0300

    entity base added to category

--- Mext-Project-2/Mext-Project-2.Domain/Entities/Category.cs
+++ Mext-Project-2/Mext-Project-2.Domain/Entities/Category.cs
@@ -1,7 +1,9 @@
 using System;
+using Mext_Project_2.Domain.Comman;
+
 namespace Mext_Project_2.Domain.Entities
 {
-  public class Category
+  public class Category:EntityBase
   {
     public int Id { get; set; }
     public string Name { get; set; }
