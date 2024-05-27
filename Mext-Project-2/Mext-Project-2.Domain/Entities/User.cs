c377ebae24a809a3503705d2870cb68c171ca22e
eda özdemir <88721437+eda426@users.noreply.github.com>
2024-05-27 15:27:07 +0300

    EntityBase edit user

--- Mext-Project-2/Mext-Project-2.Domain/Entities/User.cs
+++ Mext-Project-2/Mext-Project-2.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mext_Project_2.Domain.Comman;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Mext_Project_2.Domain.Entities
 {
-    public class User
+    public class User:EntityBase
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
