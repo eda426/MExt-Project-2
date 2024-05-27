2a70baaaa5b2abb67377ae291c3c6f7a3e3fb01e
bektascansel <cansel.bektas6447@gmail.com>
2024-05-27 15:15:15 +0300

    first commit

---
+++ Mext-Project-2/Mext-Project-2.Domain/Comman/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mext_Project_2.Domain.Comman
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+    }
+}
